321823160cdc2b953fd609f061a957b7abc83b61
Ludo <ludovico@rebrandly.com>
2020-02-05 10:18:07 +0100

    Docker remove volumes attached to containers

--- src/tests/integration/docker-remove-containers.sh
+++ src/tests/integration/docker-remove-containers.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 
 # Stop and remove any running container
-docker ps -a | awk -F '[ ]+' 'NR>1 {print($1)}' | xargs -n1 docker stop | xargs -n1 docker rm
+docker ps -a | awk -F '[ ]+' 'NR>1 {print($1)}' | xargs -n1 docker stop | xargs -n1 docker rm --volumes
